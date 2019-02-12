77ae29b4f7de706d9eb0a04450d60d0b740970c4
Andrade Sampaio <andrade.sampaio@outlook.com>
2019-02-12 15:40:16 -0200

    Resoluçoes dos exercicios 2 e 3

--- java-3/src/main/java/challenge/Main.java
+++ java-3/src/main/java/challenge/Main.java
@@ -1,33 +1,56 @@
 package challenge;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
+	private List<Jogador> jogadores = getJogadores();
+
 	// Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo?
 	public int q1() {
-		
-		List<Jogador> jogadores = ListaDeJogadores.getJogadores();
+
 		Set<String> nationality = new HashSet<>();
-		
-		for (Jogador jogador : jogadores) {
-			nationality.add(jogador.getNationality());
-		}
+		jogadores.stream().map(jogador -> nationality.add(jogador.getNationality())).collect(Collectors.toSet());
+
+//		for (Jogador jogador : jogadores) {
+//			nationality.add(jogador.getNationality());
+//		}
 		return nationality.size();
 	}
 
 	// Quantos clubes (coluna `club`) diferentes existem no arquivo?
 	// Obs: Existem jogadores sem clube.
 	public int q2() {
-		return 0;
+		Set<String> clubs = new HashSet<String>();
+
+		jogadores.stream().filter(jogador -> !jogador.getClub().isEmpty()).map(jogador -> clubs.add(jogador.getClub()))
+				.collect(Collectors.toSet());
+		return clubs.size();
+
 	}
 
 	// Liste o primeiro nome (coluna `full_name`) dos 20 primeiros jogadores.
 	public List<String> q3() {
-		return null;
+		
+		List<String> nomes = new ArrayList<String>();
+
+		for (int i = 0; i <= jogadores.size(); i++) {
+			if (i < 20) {
+				nomes.add(jogadores.get(i).getFull_name());
+			}
+		}
+		return nomes;
 	}
 
 	// Quem são os top 10 jogadores que possuem as maiores cláusulas de rescisão?
@@ -36,16 +59,62 @@ public class Main {
 		return null;
 	}
 
-	// Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
+	// Quem são os 10 jogadores mais velhos (use como critério de desempate o
+	// campo `eur_wage`)?
 	// (utilize as colunas `full_name` e `birth_date`)
 	public List<String> q5() {
 		return null;
 	}
 
-	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
+	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde
+	// as chaves são as idades e os valores a contagem.
 	// (utilize a coluna `age`)
 	public Map<Integer, Integer> q6() {
 		return null;
+	}
+
+	public List<Jogador> getJogadores() {
+
+		String csvFile = "C:\\Users\\andrade.sampaio\\codenation\\java-3\\src\\main\\resources\\data.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		List<Jogador> jogadores = new ArrayList<Jogador>();
+
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+
+				Jogador jogador = new Jogador();
+
+				String[] country = line.split(cvsSplitBy);
+				if (!"ID".contains(country[0])) {
+					jogador.setFull_name(country[2]);
+					jogador.setClub(country[3]);
+					jogador.setAge(Long.parseLong(country[6]));
+					jogador.setBirth_date(LocalDate.parse(country[8], formatter));
+					jogador.setNationality(country[14]);
+					jogador.setEur_wage(Double.parseDouble(country[17]));
+					jogador.setEur_release_clause(country[18]);
+					jogadores.add(jogador);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return jogadores;
+
 	}
 
 }
--- java-3/src/main/java/challenge/TryMain.java
+++ java-3/src/main/java/challenge/TryMain.java
@@ -1,23 +1,51 @@
 package challenge;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class TryMain {
 
 	public static void main(String[] args) {
-		
-		List<Jogador> jogadores = ListaDeJogadores.getJogadores();
-		Set<String> nationality = new HashSet<>();
-		
-		for (Jogador jogador : jogadores) {
-			nationality.add(jogador.getNationality());
 
+		Main main = new Main();
+
+		List<Jogador> jogadores = main.getJogadores();
+		List<String> clubs = new  ArrayList<String>();
+		
+		
+		for (int i = 0; i <= jogadores.size(); i++) {
+			
+			if(i < 20) {
+				clubs.add(jogadores.get(i).getFull_name());
+		
+		}	
+			
+		
+		}
+		clubs.forEach(System.out::println);
+        
 		
 		
 		
 		
+		
+//		jogadores.stream()
+//			.filter(jogador -> !jogador.getClub().isEmpty())
+//			.map(jogador -> clubs.add(jogador.getClub()))
+//			.collect(Collectors.toSet());
+		// jogadores.forEach(jogador -> System.out.println(jogador.getFull_name()));
+
+		/*
+		 * jogadores.forEach(jogador -> { if (!jogador.getClub().isEmpty()) {
+		 * System.out.println(jogador.getClub()); clubs.add(jogador.getClub()); } });
+		 */
+//		 for (Jogador jogador : jogadores) {
+//			 if(!"".equals(jogador.getClub())) {
+//			 clubs.add(jogador.getClub());
+//			 }
+//		}  
+
+		// clubs.forEach(System.out::println);
+		System.out.println(clubs.size());
 	}
 }
-
